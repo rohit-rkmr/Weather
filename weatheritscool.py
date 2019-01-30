f271314922803407175ce6039fa7a1901237229a
Rohit Ravikumar <42780825+rohit-rkmr@users.noreply.github.com>
2019-01-30 21:23:51 +0530

    Update weatheritscool.py

--- weatheritscool.py
+++ weatheritscool.py
@@ -1,7 +1,7 @@
 import json,requests
 cityname=input("enter city name")
 
-response=requests.get("http://api.openweathermap.org/data/2.5/weather?appid=0adc6b2eaf035393c5008f478f36447f&q="+ cityname)
+response=requests.get("http://api.openweathermap.org/data/2.5/weather?appid=<your api id>"+ cityname)
 climate=response.json( )
 print(cityname,climate['sys']['country'])
 print(climate['main']['temp']-273.15,"degree celcius")
